5f3cd9130a6920fedf50db02b1ef7507b83967c4
Mona Norremo <mona.norremo@gmail.com>
2022-04-25 15:00:46 +0200

    Created New Window Gui

--- BroGridLayout.java
+++ BroGridLayout.java
@@ -9,7 +9,7 @@ public class BroGridLayout {
 		JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 500);
-        frame.setLayout(new GridLayout(3, 3));
+        frame.setLayout(new GridLayout(3, 3, 10, 10));
         
         frame.add(new JButton("1"));
         frame.add(new JButton("2"));
@@ -20,7 +20,7 @@ public class BroGridLayout {
         frame.add(new JButton("7"));
         frame.add(new JButton("8"));
         frame.add(new JButton("9"));
-        
+        //frame.add(new JButton("10"));
 		
         
         
---
+++ BroNewWindow.java
@@ -0,0 +1,5 @@
+public class BroNewWindow {
+   public static void main(String[] args) {
+       
+   } 
+}
