120a197ca24caaf3e436e318fa8e783327404f3a
Mona Norremo <mona.norremo@gmail.com>
2022-04-25 15:31:57 +0200

    new

--- HowToOpenANewWindow/src/Main.java
+++ HowToOpenANewWindow/src/Main.java
@@ -2,9 +2,9 @@ public class Main{
 
     public static void main(String[] args) {
     
+        LaunchPage launchpage = new LaunchPage();
 
 
-        
     }
 }
 
--- HowToOpenANewWindow/src/NewWindow.java
+++ HowToOpenANewWindow/src/NewWindow.java
@@ -1,3 +1,25 @@
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.plaf.FontUIResource;
+
 public class NewWindow {
     
+    JFrame frame = new JFrame();
+    JLabel label = new JLabel("Hello!");
+   
+    NewWindow(){
+
+        label.setBounds(0, 0, 100, 50);
+        label.setFont(new FontUIResource(null, Font.PLAIN, 25));
+
+        frame.add(label);
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(420,420);
+    frame.setLayout(null);
+    frame.setVisible(true);
+
+    }
 }
